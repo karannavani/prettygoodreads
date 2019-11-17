4e2a8c699519ad50c686140b7d29b9aaf98f00f9
Karan Navani <karannavani@deloitte.co.uk>
2019-11-17 21:53:15 +0000

    books show up with images, for the most poart

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ const key = 'yPse7ekZ2yogdr6dKky12w&v=2'
 fetchBooks();
 
 function fetchBooks() {
-    shelves = ['currently-reading', 'to-read', 'read']
+    // shelves = ['currently-reading', 'to-read', 'read']
+    shelves = ['currently-reading']
     shelves.forEach(shelf => {
         url = `https://cors-anywhere.herokuapp.com/https://www.goodreads.com/review/list/58929869.xml?key=${key}&shelf=${shelf}&per_page=200&page=1`
 
@@ -14,23 +15,48 @@ function fetchBooks() {
                 return new window.DOMParser().parseFromString(string, 'text/xml')
             })
             .then(result => {
-
-                toRead = result.getElementsByTagName('reviews')[0].children;
-                return getBookTitles(toRead);
+                books = result.getElementsByTagName('reviews')[0].children;
+                // getBookTitles(books);
+                return handleBooks(shelf, books);
             });
 
     })
 }
 
+function handleBooks(shelf, books) {
 
+    for (let i = 0; i < books.length; i++) {
+        console.log(books[i].children[1]);
 
-function getBookTitles(toRead) {
+        book = {
+            title: books[i].children[1].children[5].innerHTML,
+            imageUrl: books[i].children[1].children[7].innerHTML,
+            isbn: books[i].children[1].children[2].innerHTML
+        }
 
-    for (let i = 0; i < toRead.length; i++) {
-        // console.log(toRead[i].children[1].children[5]);
+        displayRead(book);
 
-        let title = toRead[i].children[1].children[5].innerHTML;
-
-        console.log(title);
+        // console.log(title);
     };
-};
+    
+}
+
+function displayRead(book) {
+
+    currentDiv = $('.currently-reading');
+    currentDiv.append(`<p>${book.title}</p>`);
+    currentDiv.append(`<img src= ${book.imageUrl}>`);
+}
+
+// function getBookTitles(books) {
+
+//     for (let i = 0; i < books.length; i++) {
+//         // console.log(books[i].children[1].children[7].innerHTML);
+
+//         let title = books[i].children[1].children[5].innerHTML;
+
+//         displayRead(title);
+
+//         // console.log(title);
+//     };
+// };
