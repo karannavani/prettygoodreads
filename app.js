9259d39078b88d95711ec181b46dd478cf3dc0d4
Karan Navani <kknavani@gmail.com>
2019-11-23 11:14:52 +0000

    Added some improvements

--- app.js
+++ app.js
@@ -1,52 +1,48 @@
-
-const key = 'yPse7ekZ2yogdr6dKky12w&v=2'
+const key = 'yPse7ekZ2yogdr6dKky12w&v=2';
 
 fetchBooks();
 
 function fetchBooks() {
-    // shelves = ['currently-reading', 'to-read', 'read']
-    shelves = ['currently-reading']
-    shelves.forEach(shelf => {
-        url = `https://cors-anywhere.herokuapp.com/https://www.goodreads.com/review/list/58929869.xml?key=${key}&shelf=${shelf}&per_page=200&page=1`
+  // shelves = ['currently-reading', 'to-read', 'read']
+  const shelves = ['currently-reading'];
+  shelves.forEach(shelf => {
+    const url = `https://cors-anywhere.herokuapp.com/https://www.goodreads.com/review/list/58929869.xml?key=${key}&shelf=${shelf}&per_page=200&page=1`;
 
-        fetch(url)
-            .then(res => res.text())
-            .then(string => {
-                return new window.DOMParser().parseFromString(string, 'text/xml')
-            })
-            .then(result => {
-                books = result.getElementsByTagName('reviews')[0].children;
-                // getBookTitles(books);
-                return handleBooks(shelf, books);
-            });
-
-    })
+    fetch(url)
+      .then(res => res.text())
+      .then(string => {
+        return new window.DOMParser().parseFromString(string, 'text/xml');
+      })
+      .then(result => {
+        const books = result.getElementsByTagName('reviews')[0].children;
+        // getBookTitles(books);
+        return handleBooks(shelf, books);
+      });
+  });
 }
 
 function handleBooks(shelf, books) {
+  for (let i = 0; i < books.length; i++) {
+    console.log(books[i].children[1]);
 
-    for (let i = 0; i < books.length; i++) {
-        console.log(books[i].children[1]);
-
-        book = {
-            title: books[i].children[1].children[5].innerHTML,
-            imageUrl: books[i].children[1].children[7].innerHTML,
-            isbn: books[i].children[1].children[2].innerHTML
-        }
-
-        displayRead(book);
-
-        // console.log(title);
+    const book = {
+      title: books[i].children[1].children[5].innerHTML,
+      author: books[i].children[1].children[21].children[0].children[1].innerHTML,
+      isbn: books[i].children[1].children[2].innerHTML
     };
-    
+
+    displayRead(shelf, book);
+
+    // console.log(title);
+  }
 }
 
-function displayRead(book) {
-
-    scrollDiv = $('.scrolling-wrapper-flexbox');
-    scrollDiv.append(`<div class="current-book col">
-    <img src= ${book.imageUrl}>
+function displayRead(shelf, book) {
+  const currentlyReading = $(`.${shelf}`);
+  currentlyReading.append(`
+  <div class='current-book col-5 align-middle'>
     <p>${book.title}</p>
+    <p>By ${book.author}</p>
     </div>`);
 }
 
